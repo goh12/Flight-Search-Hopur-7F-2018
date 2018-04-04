58552e23d5e562155399a4a5cc30cbe09f3c1dc7
goh12 <goh12@hi.is>
2018-04-04 15:38:12 +0000

    netbeans hints í BookingsView

--- test/containers/FlightsTest.java
+++ test/containers/FlightsTest.java
@@ -12,9 +12,7 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
